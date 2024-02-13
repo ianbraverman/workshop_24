88aa796aabae5abc2c18b2154f074eb0aa8eab9b
Ian <ianbraverman@hotmail.com>
2024-02-13 15:33:28 -0500

    finished the third tab of the workshop

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,28 @@ import { useState } from 'react'
 
 function App() {
 const [puppies, setPuppies] = useState(puppyList)
+const [featPupId, setFeatPupId] = useState(null)
 console.log("Puppy List: ", puppyList)
+const featuredPup = puppies.find((pup)=> pup.id === featPupId)
   return (
     <div className = "App">
       {
         puppies.map((puppy) => {
-          return <p>{puppy.name}</p>
+
+          console.log(featuredPup)
+          return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
         })
       }
+      { featPupId && (
+      <div>
+        <h2> {featuredPup.name} </h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>
+      )}  
+        
     </div>
   );
 }
