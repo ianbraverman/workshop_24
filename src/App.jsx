7550cb633900ee388a90171b91df79fe01b327a9
Ian <ianbraverman@hotmail.com>
2024-02-13 15:04:32 -0500

    finished second tab of the workshop.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { puppyList } from './data.js'
+import { useState } from 'react'
 
 function App() {
-
+const [puppies, setPuppies] = useState(puppyList)
+console.log("Puppy List: ", puppyList)
   return (
-    <>
-      
-    </>
-  )
+    <div className = "App">
+      {
+        puppies.map((puppy) => {
+          return <p>{puppy.name}</p>
+        })
+      }
+    </div>
+  );
 }
 
 export default App
